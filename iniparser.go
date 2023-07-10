d1f16afd57cfecb1744735fbbddbc30b7b63cba7
diaabadr <diaabadr82@gmail.com>
2023-07-10 16:10:15 +0300

    Documenting the apis

--- iniparser.go
+++ iniparser.go
@@ -10,7 +10,6 @@ import (
 	"strings"
 )
 
-// exported variable so it should start with uppercase
 var (
 	ErrInvalidFormat    = errors.New("invalid format ")
 	ErrInvalidExtension = errors.New("file is not in the ini format or does not have a .ini extension")
@@ -26,34 +25,36 @@ type Parser struct {
 	sections Ini
 }
 
-// NewParser returns new Parser
+// NewParser returns a new Parser object.
 func NewParser() Parser {
 	return Parser{sections: make(Ini)}
 }
 
+// LoadFromString loads INI data from a string.
 func (p *Parser) LoadFromString(content string) error {
 	return p.LoadFromReader(bufio.NewReader(strings.NewReader(content)))
 }
 
+// LoadFromFile loads INI data from a file.
 func (p *Parser) LoadFromFile(path string) error {
 
+	// Check file extension
 	fileExt := filepath.Ext(path)
-
 	if fileExt != ".ini" {
 		return ErrInvalidExtension
 	}
 
+	// Open file
 	file, err := os.Open(path)
-
 	if err != nil {
 		return err
 	}
-
 	defer file.Close()
 
 	return p.LoadFromReader(bufio.NewReader(file))
 }
 
+// LoadFromReader loads INI data from an io.Reader object.
 func (p *Parser) LoadFromReader(reader io.Reader) error {
 	var currentSection string = ""
 	scanner := bufio.NewScanner(reader)
@@ -63,16 +64,16 @@ func (p *Parser) LoadFromReader(reader io.Reader) error {
 		line := scanner.Text()
 
 		line = strings.TrimSpace(line)
-		// comment
+
+		// Skip empty lines and comments
 		if len(line) == 0 || string(line[0]) == "#" {
 			continue
 		}
 
-		// checking if the line in that format [section] and just exist one time in the line
-
-		// new section
+		// Check for section header
 		if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
 
+			// Extract section name
 			sectionName := line[1 : len(line)-1]
 			sectionName = strings.TrimSpace(sectionName)
 
@@ -80,16 +81,17 @@ func (p *Parser) LoadFromReader(reader io.Reader) error {
 				return errors.Wrapf(ErrInvalidFormat, "invalid section at line %d", idx)
 			}
 
+			// Start new section
 			currentSection = sectionName
-
 			p.sections[currentSection] = make(Section)
 			continue
 
 		}
 
+		// Check for key-value pairs
 		if strings.Contains(line, "=") && currentSection != "" {
 
-			// key and value line
+			// Parse key and value
 			keyAndValue := strings.Split(line, "=")
 			key, value := keyAndValue[0], keyAndValue[1]
 			key, value = strings.TrimSpace(key), strings.TrimSpace(value)
@@ -98,6 +100,7 @@ func (p *Parser) LoadFromReader(reader io.Reader) error {
 				return errors.Wrapf(ErrInvalidFormat, "invalid key at line %d", idx)
 			}
 
+			// Add key-value pair to current section
 			p.sections[currentSection][key] = value
 		} else {
 			return errors.Wrapf(ErrInvalidFormat, "invalid format at line %d", idx)
@@ -105,7 +108,7 @@ func (p *Parser) LoadFromReader(reader io.Reader) error {
 	}
 	return scanner.Err()
 }
-
+// GetSectionNames returns the names of all sections in the INI data.
 func (p *Parser) GetSectionNames() []string {
 	sections := make([]string, 0, len(p.sections))
 	for section := range p.sections {
@@ -114,17 +117,21 @@ func (p *Parser) GetSectionNames() []string {
 	return sections
 }
 
+// GetSections returns the entire INI data as a map of sections to key-value pairs.
 func (p *Parser) GetSections() Ini {
 	return p.sections
 }
 
+// Get returns the value of a key in a section.
 func (p *Parser) Get(section_name, key string) (string, error) {
 
+	// Check if section exists
 	_, ok := p.sections[section_name]
 	if !ok {
 		return "", ErrSectionNotExist
 	}
 
+	// Check if key exists
 	value, ok := p.sections[section_name][key]
 	if !ok {
 		return "", ErrKeyNotExist
@@ -133,14 +140,19 @@ func (p *Parser) Get(section_name, key string) (string, error) {
 	return value, nil
 }
 
+// Set sets the value of a key in a section.
 func (p *Parser) Set(section_name, key, value string) {
 
+	// Create section if it doesn't exist
 	if p.sections[section_name] == nil {
 		p.sections[section_name] = make(Section)
 	}
+
+	// Set key-value pair
 	p.sections[section_name][key] = value
 }
 
+// String returns the INI data in string format.
 func (p *Parser) String() string {
 
 	configText := ""
@@ -154,15 +166,14 @@ func (p *Parser) String() string {
 	return configText
 }
 
+// SaveToFile saves the INI data to a file.
 func (p *Parser) SaveToFile(path string) error {
 
+	// Get INI data as string
 	configString := p.String()
 	stringBytes := []byte(configString)
 
-	// 0644 is an octal code for access (admin: read and write, other users :read)
+
+	// 0644 is an octal code for access (Owner: read and write, Members of the file's group and other users : read)
 	return os.WriteFile(path, stringBytes, 0644)
-
 }
-
-// adding new case to test Get
-// new test with 2 sections in one line []
